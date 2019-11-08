6020ed81feb7e85daa6aae4a16fdeb39829db7dd
palewing <rendo.030@gmail.com>
2019-11-08 10:08:10 +0700

    scare page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
     <div>
+      <div v-if="jumpScare">
+        <scarepage></scarepage>
+      </div>
+      <div>
         <homepage></homepage>
+      </div>
     </div>
 </template>
 
 <script>
-import homepage from "./views/homepage";
+import homepage from "./views/homePage";
+import scarepage from "./views/scarePage"
 export default {
     components: {
-        homepage
+        scarepage
+    },
+    data: function() {
+    return {
+      linkSeram: "",
+      jumpScare: false
+    };
+  },
+  props: [],
+  methods: {
+    getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return "";
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
+  },
+  created() {
+    this.linkSeram = this.getParameterByName("q");
+    if (this.linkSeram) {
+      this.jumpScare = true;
+    }
+  }
 }
 </script>
 
---
+++ src/views/scarePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <audio autoplay src="../../assets/horse.ogg"></audio>
+      <img
+        src="https://cdn.lynda.com/course/542431/542431-636251707678601607-16x9.jpg"
+        style="width: 100%; height: 100vh;"
+      />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "scarepage",
+}
+</script>
+
+<style>
+</style>
