bdf32681c32c0b780af0329bfe94ddb3deed59a6
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-11-08 11:38:14 +0700

    link to detail

--- src/components/contentPost.vue
+++ src/components/contentPost.vue
@@ -14,7 +14,7 @@
       <!--ROW for the image-->
       <div class="row">
         <div class="col-12 imgDiv">
-            <img
+            <img @click="seeDetail"
               :src="urlImage"
               class="img-content img-fluid"
             />
@@ -36,7 +36,35 @@
 <script>
 export default {
     name: "contentPost",
-    props: ['urlImage']
+    props: ['urlImage', 'idImage'],
+    methods: {
+      insertParam(key, value) {
+        key = encodeURI(key); value = encodeURI(value);
+
+        var kvp = document.location.search.substr(1).split('&');
+
+        var i=kvp.length; var x; while(i--) 
+        {
+            x = kvp[i].split('=');
+
+            if (x[0]==key)
+            {
+                x[1] = value;
+                kvp[i] = x.join('=');
+                break;
+            }
+        }
+
+        if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+        //this will reload the page, it's likely better to store this until finished
+        document.location.search = kvp.join('&'); 
+      },
+      seeDetail() {
+        console.log(this.idImage)
+        window.history.pushState("object or string", "Title", `?q=${this.idImage}`);
+      }
+    }
 };
 </script>
 
